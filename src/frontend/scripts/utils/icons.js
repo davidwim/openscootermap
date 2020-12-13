d40d8efbd26996112f0f27609f68b4363c21d489
David <63693881+davidwim@users.noreply.github.com>
2020-12-13 20:49:44 +1300

    Update icons.js

--- src/frontend/scripts/utils/icons.js
+++ src/frontend/scripts/utils/icons.js
@@ -1,7 +1,7 @@
 import * as L from 'leaflet';
 const iconBase = {
-    iconSize: [25, 25],
-    iconAnchor: [12, -1]
+    iconSize: [39, 46.5],
+    iconAnchor: [20, 47.5]
 };
 
 export const limeIcon = new L.Icon({
@@ -26,6 +26,25 @@ export const birdIcon = new L.Icon({
 
 export const flamingoIcon = new L.Icon({
     iconUrl: require("../../assets/flamingo.png"),
-    iconSize: [25, 39],
-    iconAnchor: [12, -1]
+    ...iconBase
+});
+
+export const lchargeIcon = new L.Icon({
+    iconUrl: require("../../assets/lcharge.png"),
+    ...iconBase
+});
+
+export const jumpIcon = new L.Icon({
+    iconUrl: require("../../assets/jump.png"),
+    ...iconBase
+});
+
+export const lhubIcon = new L.Icon({
+    iconUrl: require("../../assets/lhub.png"),
+    ...iconBase
+});
+
+export const blipIcon = new L.Icon({
+    iconUrl: require("../../assets/blip.png"),
+    ...iconBase
 });
